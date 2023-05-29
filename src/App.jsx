851a044aef6fa5e995272ccb395f620fe885e46d
Carmen <path4design@gmail.com>
2023-05-29 12:43:45 +0200

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import "./App.css";
+useState
 
 function randomNumber() {
   return Math.floor(Math.random() * 11);
@@ -6,24 +8,45 @@ function randomNumber() {
 
 export default function App() {
 
+  const [firstNumber, setfirstNumber] = useState(randomNumber())
+  const [secondNumber, setsecondNumber] = useState(randomNumber())
+  const [inputNumber, setInputNumber] = useState("")
+  const [showMessage, setShowMessage] = useState(false)
+
+  // Calculo la variable isCorrect en función de las variables de estado. Yo tengo la certeza que cada vez que se cambia una variable de estado, esta instrucción se vuelve a ejecutar
+  let isCorrect = firstNumber + secondNumber === +inputNumber
+
+  function handleClick(){
+    setShowMessage(true)
+  }
+
   return (
     <div className="App">
       <div id="canvas">
-        <div id="primary-number">10</div>
+        <div id="primary-number">{firstNumber}</div>
         <div id="add">+</div>
-        <div id="secondary-number">7</div>
+        <div id="secondary-number">{secondNumber}</div>
         <div id="equal">=</div>
         <div>
           <input
             type="number"
             id="guess"
+            value={inputNumber}
+            onChange={(e) => setInputNumber(e.target.value) }
           />
         </div>
         <div>
-          <button id="btn" >
+        {/* mini-reto. Codifica los eventos necesarios y funciones, para que al hacer click en "Check", me muestre POR CONSOLA si la suma es correcta o no. NO HACE FALTA MÁS VARIABLES DE ESTADO */}
+          <button id="btn" onClick={handleClick}>
             Check
           </button>
         </div>
+       
+      </div>
+      <div>
+      {isCorrect && <p className="correct">El resultado es correcto</p>}
+      {!isCorrect && <p className="incorrect">El resultado es incorrecto</p>}
+      {/* {isCorrect ? <p>El resultado es correcto</p> : <p>El resultado es incorrecto</p>} */}
       </div>
     </div>
   );
